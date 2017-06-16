ee55272b818c0b715212dc0a1df64d81398ad4ff
kenbisy8 <soichi1130@gmail.com>
2017-06-16 20:29:35 +0900

    Change user_name

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 lock "3.8.1"
 
 set :application, "Airbnb"
-set :repo_url, 'git@github.com:kenbishy8/Airbnb.git'
+set :repo_url, 'git@github.com:kenbisy8/Airbnb.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: 'publickey',
+set :ssh_options, auth_methods: ['publickey'],
                   keys: '/Users/soichiyasuda/.ssh/kenbishy8.pem'
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
