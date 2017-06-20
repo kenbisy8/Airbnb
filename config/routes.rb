e877a0140fc346f6184ec27db4d3856acbe7b6bc
kenbisy8 <soichi1130@gmail.com>
2017-06-20 20:43:14 +0900

    Change routing & rooms.controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :rooms, only: :index
+  resources :rooms, only: [:index, :new, :create, :show] do
+    resources :orders, only: [:new, :create]
+  end
   root 'rooms#index'
 end
