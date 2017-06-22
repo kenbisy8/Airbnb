93f800c9956b9acbb582841093aea94c08ae45c4
kenbisy8 <soichi1130@gmail.com>
2017-06-22 19:40:57 +0900

    Uninstall gem 'devise'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621113035) do
+ActiveRecord::Schema.define(version: 20170622103554) do
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "persons"
