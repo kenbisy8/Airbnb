1b8d8588ba70578ddb812015a8401ee730fee8e2
kenbisy8 <soichi1130@gmail.com>
2017-06-26 21:26:56 +0900

    Implement user edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render 'edit'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624015759) do
+ActiveRecord::Schema.define(version: 20170626031610) do
 
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "persons"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170624015759) do
     t.string   "prefecture"
     t.string   "city"
     t.string   "address"
+    t.integer  "user_id",    null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
