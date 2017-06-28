8db626eb5f17d95bd7d9d1d23261a5554542231c
kenbisy8 <soichi1130@gmail.com>
2017-06-28 11:06:03 +0900

    Set validation for error messages

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,9 @@ class Room < ApplicationRecord
   has_many :orders
   belongs_to :user
 
+  validates :room_name, :country, :prefecture, :city, :address, :country, :prefecture, :city, :address, presence: true
+  validates :guest, :bed, numericality: { only_integer: true, less_than_or_equal_to: 99 }, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 1, less_than: 999999 }, presence: true
+  validates :room_type, inclusion: ['まるまる貸切', '個室', 'シェアルーム']
+
 end
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Airbnb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| %Q(#{html_tag}).html_safe }
     config.generators do |g|
       g.assets     false
       g.helper     false
