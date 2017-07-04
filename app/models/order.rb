e2e1605a53788a1e1a5814783423cf814edfe5f2
kenbisy8 <soichi1130@gmail.com>
2017-07-04 19:19:33 +0900

    Set validation on order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
 
   belongs_to :user
   belongs_to :room
+
+  validates :persons, :check_in, :check_out, :messages, presence: true
+  validates :confirmation, acceptance: true
+
 end
