09722e3b34a496c27eb5c844f47c20e3ee7a7443
kenbisy8 <soichi1130@gmail.com>
2017-07-04 19:18:35 +0900

    Change orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,54 @@
 class OrdersController < ApplicationController
-  
+  before_action :find_room_info, :get_new_order
+
+  def new
+    if check_order_empty?(order_params) || 1 > get_date_dif(@order)
+      redirect_to room_path(params[:room_id]), alert: "入力内容に誤りがあります。"
+    elsif Date.today > (@order.check_in || @order.check_out)
+      redirect_to room_path(params[:room_id]), alert: "過去の日付は選択できません。"
+    else
+      get_date_dif(@order)
+      get_price(@room)
+      get_all_price(@order)
+    end
+  end
+
+  def create
+    if @order.save
+      redirect_to root_path, notice: "予約が完了いたしました。"
+    else
+      redirect_to new_room_order_path(order: order_params), flash: { error: @order.errors.full_messages }
+    end
+  end
+
+  private
+
+  def find_room_info
+    @room = Room.find(params[:room_id])
+  end
+
+  def get_new_order
+    @order = Order.new(order_params)
+  end
+
+  def get_date_dif(order)
+    @dif = order.check_out - order.check_in
+    @dif = @dif.to_i
+  end
+
+  def get_price(room)
+    @price = (room.price * 1.08).round
+  end
+
+  def get_all_price(order)
+    @prices = (@price * order.persons * @dif).round
+  end
+
+  def check_order_empty?(order)
+    order[:persons].empty? ||(order[:check_in].empty?) || (order[:check_out].empty?)
+  end
+
+  def order_params
+    params.require(:order).permit(:persons, :all_price, :check_in, :check_out, :messages, :confirmation).merge(room_id: params[:room_id], user_id: current_user.id)
+  end
 end
