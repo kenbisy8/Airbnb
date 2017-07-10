2d1a94b6111add0ba81cc3debede69c9a0d8cdc2
kenbisy8 <soichi1130@gmail.com>
2017-07-10 15:27:06 +0900

    Make Diary model & configure routing

--- db/migrate/20170706114849_create_diaries.rb
+++ db/migrate/20170706114849_create_diaries.rb
@@ -2,10 +2,11 @@ class CreateDiaries < ActiveRecord::Migration[5.0]
   def change
     create_table :diaries do |t|
       t.text :article, null: false
+      t.string :article_title, null: false
+      t.string :article_image
       t.string :nation_name, null: false
       t.string :city_name, null: false
-      t.string :article_image
-      t.boolean :public, null: false
+      t.boolean :publicity, null: false
       t.integer :article_user_id, null: false
       t.timestamps
     end
